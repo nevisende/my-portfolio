bf62258d9f13db107840ea78a08ea64ea206ee74
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2021-11-08 22:04:01 +0300

    Add requirements for PR

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
+/*  eslint-disable linebreak-style  */
 const hamburgerMenu = document.getElementById('hamburger-menu');
 const headerList = document.getElementById('header-list');
 const menuIcon = document.getElementById('menu-icon');
 const itemsHeader = document.querySelectorAll('.header-item');
 
-menuIcon.addEventListener('click', () => {
-    toogleMenu()
-})
-
-itemsHeader.forEach(item => {
-    item.addEventListener('click', () => {
-        toogleMenu()
-    })
-})
-
 function toogleMenu() {
-    headerList.classList.toggle('header-list')
-    hamburgerMenu.classList.toggle('hamburger-menu')
+  headerList.classList.toggle('header-list');
+  hamburgerMenu.classList.toggle('hamburger-menu');
+  if (menuIcon.classList.contains('fa-bars')) {
+    menuIcon.classList.remove('fa-bars');
+    menuIcon.classList.add('fa-times');
+  } else {
+    menuIcon.classList.remove('fa-times');
+    menuIcon.classList.add('fa-bars');
+  }
 }
+
+menuIcon.addEventListener('click', () => {
+  toogleMenu();
+});
+
+itemsHeader.forEach((item) => {
+  item.addEventListener('click', () => {
+    toogleMenu();
+  });
+});
