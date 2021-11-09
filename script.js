01b17901d4dc6726aa0ac6e3321d35c41ba1b5a3
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2021-11-09 18:23:39 +0300

    Add array for popup

--- script.js
+++ script.js
@@ -6,6 +6,61 @@ const itemsHeader = document.querySelectorAll('.header-item');
 const projectButtons = document.querySelectorAll('.project-button');
 const popupContainer = document.querySelector('.popup-container');
 const closePopup = document.getElementById('close-popup');
+const popupTitle = document.getElementById('popup-title');
+const popupImg = document.querySelector('.popup-img');
+const popupImgList = document.querySelector('.images');
+const popupP = document.getElementById('popup-p');
+const liveUrlButton = document.getElementById('live-url');
+const sourceUrlButton = document.getElementById('source-url');
+
+const projects = [{
+  name: 'Project name goes here',
+  tecnologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi expedita quos velit magni? Porro eos libero, numquam neque nisi facere eum modi a quos autem, facilis nihil hic at aliquid eaque error molestiae sit voluptatum assumenda beatae nostrum. Quas, delectus',
+  images: ['.media/project-1.png'],
+  liveUrl: 'htttp://hackon.me',
+  sourceUrl: 'https://github.com/nevisende/my-portfolio',
+},
+{
+  name: 'Project name goes here',
+  tecnologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi expedita quos velit magni? Porro eos libero, numquam neque nisi facere eum modi a quos autem, facilis nihil hic at aliquid eaque error molestiae sit voluptatum assumenda beatae nostrum. Quas, delectus',
+  images: ['.media/project-2.png'],
+  liveUrl: 'htttp://hackon.me',
+  sourceUrl: 'https://github.com/nevisende/my-portfolio',
+},
+{
+  name: 'Project name goes here',
+  tecnologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi expedita quos velit magni? Porro eos libero, numquam neque nisi facere eum modi a quos autem, facilis nihil hic at aliquid eaque error molestiae sit voluptatum assumenda beatae nostrum. Quas, delectus',
+  images: ['.media/project-3.png'],
+  liveUrl: 'htttp://hackon.me',
+  sourceUrl: 'https://github.com/nevisende/my-portfolio',
+},
+{
+  name: 'Project name goes here',
+  tecnologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi expedita quos velit magni? Porro eos libero, numquam neque nisi facere eum modi a quos autem, facilis nihil hic at aliquid eaque error molestiae sit voluptatum assumenda beatae nostrum. Quas, delectus',
+  images: ['.media/project-4.png'],
+  liveUrl: 'htttp://hackon.me',
+  sourceUrl: 'https://github.com/nevisende/my-portfolio',
+},
+{
+  name: 'Project name goes here',
+  tecnologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi expedita quos velit magni? Porro eos libero, numquam neque nisi facere eum modi a quos autem, facilis nihil hic at aliquid eaque error molestiae sit voluptatum assumenda beatae nostrum. Quas, delectus',
+  images: ['.media/project-5.png'],
+  liveUrl: 'htttp://hackon.me',
+  sourceUrl: 'https://github.com/nevisende/my-portfolio',
+},
+{
+  name: 'Project name goes here',
+  tecnologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi expedita quos velit magni? Porro eos libero, numquam neque nisi facere eum modi a quos autem, facilis nihil hic at aliquid eaque error molestiae sit voluptatum assumenda beatae nostrum. Quas, delectus',
+  images: ['.media/project-6.png'],
+  liveUrl: 'htttp://hackon.me',
+  sourceUrl: 'https://github.com/nevisende/my-portfolio',
+}];
 
 function toogleMenu() {
   headerList.classList.toggle('header-list');
@@ -31,10 +86,13 @@ itemsHeader.forEach((item) => {
 
 projectButtons.forEach((projectButton) => {
   projectButton.addEventListener('click', () => {
+    const projectNumber = projectButton.value;
+    popupTitle.innerText = projects[projectNumber - 1].name;
     popupContainer.style.display = 'flex';
+    
   });
 });
 
-closePopup.addEventListener('click',  () => {
+closePopup.addEventListener('click', () => {
   popupContainer.style.display = 'none';
 });
